094b539dba5c991ff6f00b0c8dd44e0c521fd0b8
mitch-fussell <mitch.fussell@outlook.com>
2026-01-15 00:56:02 +0000

    created gradio interface

--- fetch.py
+++ fetch.py
@@ -1,20 +1,34 @@
 import sqlite3
+import gradio as gr
+import pandas as pd
 
-conn = sqlite3.connect('my_database.db')
-cursor = conn.cursor()
+def fetch_points():
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
 
-query = """
-    SELECT *
-    FROM points;
+    query = """
+        SELECT *
+        FROM points;
 
-"""
+    """
 
-cursor.execute(query)
-#No commit function because you are extracting from the database
-result = cursor.fetchall() #fetches all points instead
-
-
-conn.close()
+    cursor.execute(query)
+    #No commit function because you are extracting from the database
+    result = cursor.fetchall() #fetches all points instead
 
 
-print(result)
+    conn.close()
+
+    df = pd.DataFrame(result, columns = ['id', 'x', 'y'])
+
+
+    return df
+
+iface = gr.Interface(
+    fn = fetch_points,
+    inputs = [],
+    outputs = gr.LinePlot(x = 'x', y = 'y', x_lim = (0,7), y_lim = (0,15))
+
+)
+
+iface.launch()
---
+++ side_topics.py
@@ -0,0 +1,25 @@
+# import pandas as pd
+
+# my_list = [(1,2),(3,4)]
+
+# df = pd.DataFrame(my_list, columns = ['x', 'y'])
+
+# print(df)
+
+#------------------------------------------------------------------------
+
+
+
+import gradio as gr
+
+def f(x):
+    return x**2
+
+iface = gr.Interface(
+    fn = f, #function
+    inputs = gr.Number(),
+    outputs = gr.Number()
+
+)
+
+iface.launch() #control C will close the port with the application
